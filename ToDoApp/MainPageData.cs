a370a902fdee5c475d2fcb47b74a2c980208b44c
MJM Builds <database2@flurryfestival.org>
2020-01-16 18:46:32 -0500

    Removed MainPageDataSample class
    Removed MainPageDataSample class from MainPageData.cs as it is no longer needed for testing.

--- ToDoApp/MainPageData.cs
+++ ToDoApp/MainPageData.cs
@@ -76,44 +76,4 @@ namespace ToDoApp
             ToDoCategories = new ObservableCollection<ToDoCategory>();
         }
     }
-
-    public sealed partial class MainPageDataSample : INotifyPropertyChanged
-    {
-        public ObservableCollection<string> TempList { get; set; }
-
-        private ObservableCollection<ToDoCategory> toDoCategories; // Collection of ToDo Categories and Items
-        public ObservableCollection<ToDoCategory> ToDoCategories
-        {
-            get => toDoCategories;
-            set
-            {
-                toDoCategories = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        public string TitlebarText { get; set; } = "Temp Data";
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-        public MainPageDataSample()
-        {
-            ToDoCategories = new ObservableCollection<ToDoCategory>();
-            ToDoCategories.Add(new ToDoCategory("Category 1"));
-            ToDoCategories.Add(new ToDoCategory("Category 2"));
-            ToDoCategories[0].ToDoItems.Add(new ToDoItem("Cat1 Item 1", "Blah blah blah", ItemType.Toggled));
-            ToDoCategories[0].ToDoItems.Add(new ToDoItem("Cat1 Item 2", "Blah blah blah", ItemType.Toggled));
-            ToDoCategories[0].ToDoItems.Add(new ToDoItem("Cat1 Item 3", "Blah blah blah", ItemType.Toggled));
-            ToDoCategories[1].ToDoItems.Add(new ToDoItem("Cat2 Item 1", "Blah blah blah", ItemType.Toggled));
-            ToDoCategories[1].ToDoItems.Add(new ToDoItem("Cat2 Item 2", "Blah blah blah", ItemType.Toggled));
-            ToDoCategories[1].ToDoItems.Add(new ToDoItem("Cat2 Item 3", "Blah blah blah", ItemType.Toggled));
-
-            TempList = new ObservableCollection<string>();
-            TempList.Add("Item 1");
-            TempList.Add("Item 2");
-            TempList.Add("Item 3");
-        }
-    }
 }
